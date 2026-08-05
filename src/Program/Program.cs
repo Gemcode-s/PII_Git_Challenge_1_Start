bcfdc39e4549d83aea56b2229552ca6014d590b1
Tomas <urbartom@gmail.com>
2026-08-05 10:41:24 -0300

    Correccion rama main

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,18 +8,3 @@
         Console.WriteLine(Division.Divide(7, 8));
     }
 }
-<<<<<<< HEAD
-public class Addition
-{
-   public static int Add(int a, int b)
-   {
-       return a + b;
-=======
-public class Subtraction
-{
-   public static int Subtract(int a, int b)
-   {
-       return a - b;
->>>>>>> 10129db2b2a774c8f1edabe115b8c363338a25fb
-   }
-}
